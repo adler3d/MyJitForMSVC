0a64cbf1f593fced51716520fefd90510ab0af58
adler3d <adler3d@gmail.com>
2017-01-04 17:51:51 +0300

    Update jit_build.bat

--- jit_build.bat
+++ jit_build.bat
@@ -1,2 +1,33 @@
+@if exist "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat" (
+    @call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat"
+    @echo MSVC2010
+    @goto ok
+) else (
+  @if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat" (
+      @call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\vsvars32.bat"
+      @echo MSVC2011
+      @goto ok
+  ) else (
+    @if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat" (
+        @call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
+        @echo MSVC2012
+        @goto ok
+    ) else (
+      @if exist "C:\Program Files (x86)\Microsoft Visual Studio 13.0\Common7\Tools\vsvars32.bat" (
+          @call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\Common7\Tools\vsvars32.bat"
+          @echo MSVC2013
+          @goto ok
+      ) else (
+          @echo "MSVC2013 not found "
+          @goto fail
+      )
+    )
+  )
+)
+
 @call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\vsvars32.bat"
+:ok
 @cl /nologo /EHsc jit_main.cpp
+:fail
+:end
+exit /b 2
